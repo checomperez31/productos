db7a5251ef9523a0059787e3eaad612d736d6ce6
SERGIO <checomperez@gmail.com>
2022-06-15 20:32:11 -0500

    Form validations

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:productos/providers/login_form_provider.dart';
 import 'package:productos/ui/input_decorations.dart';
 import 'package:productos/widgets/widgets.dart';
+import 'package:provider/provider.dart';
 
 class LoginScreen extends StatelessWidget {
    
@@ -17,11 +19,14 @@ class LoginScreen extends StatelessWidget {
               CardContainer(
                 child: Column(
                   children: [
-                    SizedBox(height: 10),
+                    const SizedBox(height: 10),
                     Text( 'Login', style: Theme.of(context).textTheme.headline4 ),
-                    SizedBox(height: 30),
+                    const SizedBox(height: 30),
 
-                    _LoginForm()
+                    ChangeNotifierProvider(
+                      create: ( _ ) => LoginFormProvider(),
+                      child: const _LoginForm(),
+                    )
                   ],
                 ),
               ),
@@ -44,8 +49,12 @@ class _LoginForm extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final loginForm = Provider.of<LoginFormProvider>(context);
+
     return Container(
       child: Form(
+        key: loginForm.formKey,
+        autovalidateMode: AutovalidateMode.onUserInteraction,
         child: Column(
           children: [
             TextFormField(
@@ -56,6 +65,12 @@ class _LoginForm extends StatelessWidget {
                 labelText: 'Correo electronico',
                 prefixIcon: Icons.alternate_email_sharp
               ),
+              onChanged: ( value ) => loginForm.email = value,
+              validator: (value) {
+                String pattern = r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
+                RegExp regExp  = RegExp(pattern);
+                return regExp.hasMatch(value ?? '') ? null : 'El valor no esta bien';
+              },
             ),
             const SizedBox(height: 30),
             TextFormField(
@@ -66,19 +81,31 @@ class _LoginForm extends StatelessWidget {
                 labelText: 'Contrasenia',
                 prefixIcon: Icons.lock_outline
               ),
+              onChanged: ( value ) => loginForm.password = value,
+              validator: (value) {
+                if ( value != null && value.length >= 6 ) return null;
+                return 'El pass debe tener al menos 6 caracteres';
+              },
             ),
 
             const SizedBox(height: 40,),
 
             MaterialButton(
-              onPressed: () {},
+              onPressed: loginForm.isLoading ? null : () async {
+                FocusScope.of(context).unfocus();
+                if ( !loginForm.isValidForm() ) return;
+                loginForm.isLoading = true;
+                await Future.delayed( const Duration(seconds: 2) );
+                loginForm.isLoading = false;
+                Navigator.pushReplacementNamed(context, 'home');
+              },
               shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
               disabledColor: Colors.grey,
               elevation: 0,
               color: Colors.deepPurple,
               child: Container(
-                padding: EdgeInsets.symmetric(horizontal: 80, vertical: 15),
-                child: Text('Ingresar', style: TextStyle(color: Colors.white),),
+                padding: const EdgeInsets.symmetric(horizontal: 80, vertical: 15),
+                child: Text(loginForm.isLoading ? 'Espere' : 'Ingresar', style: TextStyle(color: Colors.white),),
               ),
             )
           ],
